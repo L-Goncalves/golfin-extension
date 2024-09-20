a758fb6af28f03c8450b18560358530f0e0b6d96
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-09-20 13:07:42 -0300

    feat: adding keyword editor

---
+++ components/KeywordEditor/KeywordEditor.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import "./KeywordEditor.scss"; // Assuming you'll style it with a SCSS file
+import { Button } from "~components/Button/Button";
+import { Storage } from "@plasmohq/storage";
+
+export const KeywordEditor = () => {
+  const [keywords, setKeywords] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+
+
+
+  useEffect(() => {
+    const fetchKeywords = async () => {
+      const storage = new Storage();
+      const keywords: any = (await storage.get('keywords')) || []; // Fallback to an empty array if no domains found
+      setKeywords(keywords);
+    };
+
+    fetchKeywords();
+
+  }, [isEditing == false]); 
+
+  
+  const handleAddDomain = async () => {
+  
+    setIsEditing(!isEditing);
+    const storage = new Storage();
+
+    const keywordsArr = keywords.split(",").map((keyword) => keyword.trim());
+
+    if(isEditing){
+        await storage.set("keywords", keywordsArr)
+    }
+   
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setKeywords(e.target.value);
+  };
+
+  return (
+    <div className="keyword-editor">
+      
+        <div className="textarea-container">
+          <textarea
+            id="keywords"
+            rows={4}
+            cols={50}
+            value={keywords}
+            onChange={handleInputChange}
+            placeholder="Escreva suas palavras-chaves aqui. ( SEPARADO POR VÍRGULA )
+            Exemplo: Cadeirada, Fora do ar, Patinhos"
+            disabled={!isEditing}
+          />
+        </div>
+      
+      <div className="button-container">
+        <Button color={"#000"} onClick={handleAddDomain}>
+          {isEditing ? "Concluir Edição" : "Editar"}
+        </Button>
+       
+      </div>
+    </div>
+  );
+};
