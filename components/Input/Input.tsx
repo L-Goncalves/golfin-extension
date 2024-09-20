f13852ed0af6bc93b115552b903ee633acdd2b42
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-09-20 13:07:18 -0300

    style: spacing in input | feat: propagate state on input with new props

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -2,18 +2,23 @@ import './Input.scss'
 
 interface IProps {
     label: string;
-    placeholder?: string;
-}
-
-export const Input = (props: IProps) => {
-  const { label, placeholder} = props;
-
-  return (
-    <div className='input-container'>
+    placeholder: string;
+    value: string;
+    onChange: (newValue: string) => void;
+  }
+  
+  export const Input = (props: IProps) => {
+    const { label, placeholder, value, onChange } = props;
+  
+    return (
+      <div className="input-container">
         <label>{label}</label>
-        <input placeholder={placeholder} />
-      
-    </div>
-    
-  );
-};
+        <input
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => onChange(e.target.value)} // Propagate change to parent
+        />
+      </div>
+    );
+  };
+  
