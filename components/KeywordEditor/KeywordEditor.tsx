7a433c56878a62c76441e93cd6c2de7b7adb1c95
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-09-22 04:37:56 -0300

    style(keyword-editor): adding icon and centralizing it

--- components/KeywordEditor/KeywordEditor.tsx
+++ components/KeywordEditor/KeywordEditor.tsx
@@ -1,70 +1,71 @@
-import { useEffect, useState } from "react";
-import "./KeywordEditor.scss"; // Assuming you'll style it with a SCSS file
-import { Button } from "~components/Button/Button";
-import { Storage } from "@plasmohq/storage";
+import { useEffect, useState } from "react"
+
+import "./KeywordEditor.scss" // Assuming you'll style it with a SCSS file
+
+import { MdEditSquare } from "react-icons/md"
+
+import { Storage } from "@plasmohq/storage"
+
+import { Button } from "~components/Button/Button"
 
 export const KeywordEditor = () => {
-  const [keywords, setKeywords] = useState("");
-  const [isEditing, setIsEditing] = useState(false);
-
-
+  const [keywords, setKeywords] = useState("")
+  const [isEditing, setIsEditing] = useState(false)
 
   useEffect(() => {
     const fetchKeywords = async () => {
-      const storage = new Storage();
-      const keywords: any = (await storage.get('keywords')) || []; // Fallback to an empty array if no domains found
-      setKeywords(keywords);
-    };
+      const storage = new Storage()
+      const keywords: any = (await storage.get("keywords")) || [] // Fallback to an empty array if no domains found
+      setKeywords(keywords)
+    }
 
-    fetchKeywords();
+    fetchKeywords()
+  }, [isEditing == false])
 
-  }, [isEditing == false]); 
-
-  
   const handleAddKeywords = async () => {
-  
-  
-    setIsEditing(!isEditing);
-    const storage = new Storage();
-  
-      const keywordsArr = keywords.split(",")
-      .map((keyword) => keyword.trim())
-      .filter((keyword) => keyword !== "");
+    setIsEditing(!isEditing)
+    const storage = new Storage()
 
-      if(isEditing){
-          await storage.set("keywords", keywordsArr)
-      }
-    
- 
-   
+    const keywordsArr = keywords
+      .split(",")
+      .map((keyword) => keyword.trim())
+      .filter((keyword) => keyword !== "")
+
+    if (isEditing) {
+      await storage.set("keywords", keywordsArr)
+    }
   }
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setKeywords(e.target.value);
-  };
+    setKeywords(e.target.value)
+  }
 
   return (
     <div className="keyword-editor">
-      
-        <div className="textarea-container">
-          <textarea
-            id="keywords"
-            rows={4}
-            cols={50}
-            value={keywords}
-            onChange={handleInputChange}
-            placeholder="Escreva suas palavras-chaves aqui. ( SEPARADO POR VÍRGULA )
+      <div className="textarea-container">
+        <textarea
+          id="keywords"
+          rows={4}
+          cols={50}
+          value={keywords}
+          onChange={handleInputChange}
+          placeholder="Escreva suas palavras-chaves aqui. ( SEPARADO POR VÍRGULA )
             Exemplo: Cadeirada, Fora do ar, Patinhos"
-            disabled={!isEditing}
-          />
-        </div>
-      
+          disabled={!isEditing}
+        />
+      </div>
+
       <div className="button-container">
         <Button onClick={handleAddKeywords}>
-          {isEditing ? "Concluir Edição" : "Editar"}
+          {isEditing ? (
+            "Concluir Edição"
+          ) : (
+            <>
+              <MdEditSquare className="icon" width={50} /> Editar{" "}
+            </>
+          )}
         </Button>
-       
       </div>
     </div>
-  );
-};
+  )
+}
