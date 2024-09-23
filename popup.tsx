19d818c366e5dba2f8f9784422596116839da22c
Lucas Gonçalves <ldgoncalvs@gmail.com>
2024-09-23 09:46:39 -0300

    fix: no-auth version of the extension

--- popup.tsx
+++ popup.tsx
@@ -1,72 +1,49 @@
-import { useEffect, useState } from "react";
-import { Storage } from "@plasmohq/storage";
-import { Button } from "~components/Button/Button";
-import { TabFeed } from "~components/TabFeed/TabFeed";
-import Tabs from "~components/Tabs/Tabs";
+import { useEffect, useState } from "react"
+
+import { Storage } from "@plasmohq/storage"
+
+import { Button } from "~components/Button/Button"
+import { TabFeed } from "~components/TabFeed/TabFeed"
+import Tabs from "~components/Tabs/Tabs"
+
 // @ts-ignore
-import icon from "./assets/icon.png";
-import { deleteAllStorage } from "./contentScripts/storage";
-import "./index.scss";
-import { InputToggle } from "~components/InputToggle/InputToggle";
-import SignIn from "~components/SignIn/SignIn";
-import { TabConnections } from "~components/TabConnections/TabConnections";
-import { TabJobs } from "~components/TabJobs/TabJobs";
-import { auth } from "./firebaseConfig"; // Import Firebase Auth
-import { onAuthStateChanged, signOut } from "firebase/auth";  // Import signOut from Firebase
-import { MdOutlineLogout } from "react-icons/md";
+import icon from "./assets/icon.png"
+import { deleteAllStorage } from "./contentScripts/storage"
+
+import "./index.scss"
+
+import { MdOutlineLogout } from "react-icons/md"
+
+import { InputToggle } from "~components/InputToggle/InputToggle"
+import { TabConnections } from "~components/TabConnections/TabConnections"
+import { TabJobs } from "~components/TabJobs/TabJobs"
 
 function IndexPopup() {
-  const [isExtensionEnabled, setIsExtensionEnabled] = useState(false);
-  const [authState, setAuthState] = useState<boolean | null>(null); // Null means loading
+  const [isExtensionEnabled, setIsExtensionEnabled] = useState(false)
 
   useEffect(() => {
-    // Check if user is authenticated
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user && user.emailVerified) {
-        setAuthState(true); // User is authenticated
-      } else {
-        setAuthState(false); // User is not authenticated
-      }
-    });
-
     const fetchStorageData = async () => {
-      const storage = new Storage();
-      const storedEnabledState = await storage.get("enabled");
-      const isEnabledValue = !!storedEnabledState;
-      setIsExtensionEnabled(isEnabledValue);
-    };
+      const storage = new Storage()
+      const storedEnabledState = await storage.get("enabled")
+      const isEnabledValue = !!storedEnabledState
+      setIsExtensionEnabled(isEnabledValue)
+    }
 
-    fetchStorageData();
-
-    // Cleanup the auth listener on component unmount
-    return () => unsubscribe();
-  }, []);
+    // Check if user is authenticated
+    fetchStorageData()
+  })
 
   const tabData = [
     { id: "tab1", label: "Feed", content: <TabFeed /> },
     { id: "tab2", label: "Vagas", content: <TabJobs /> },
-    { id: "tab3", label: "Minha Rede", content: <TabConnections /> },
-  ];
+    { id: "tab3", label: "Minha Rede", content: <TabConnections /> }
+  ]
 
   const handleToggleChange = async (enabled: boolean) => {
-    setIsExtensionEnabled(enabled);
-    const storage = new Storage();
-    await storage.set("enabled", enabled);
-  };
-
-  const handleAuthSuccess = () => {
-     setAuthState(true); // Set auth to true once the user logs in successfully
-  };
-
-  const handleSignOut = async () => {
-    try {
-      await signOut(auth);  // Sign out the user
-      setAuthState(false);  // Update the state to reflect logged-out status
-      // await deleteAllStorage();  // Optionally clear all storage related to the extension
-    } catch (error) {
-      console.error("Error signing out:", error);
-    }
-  };
+    setIsExtensionEnabled(enabled)
+    const storage = new Storage()
+    await storage.set("enabled", enabled)
+  }
 
   return (
     <div className="container">
@@ -78,33 +55,16 @@ function IndexPopup() {
         <h4>A sua extensão para o LinkedIn!</h4>
       </div>
 
-      {authState === null ? (
-        // Loading state
-        <div>Carregando...</div>
-      ) : authState ? (
-        // Show the main content if authenticated
-        <>
-          <InputToggle
-            label="Habilitar extensão"
-            value={isExtensionEnabled}
-            onChange={handleToggleChange}
-          />
-          <Tabs tabs={tabData} />
-
-
-          <div className="sign-out-button">
-            <Button onClick={handleSignOut}>
-            <MdOutlineLogout />  Sair
-            </Button>
-          </div>
-     
-        </>
-      ) : (
-     
-        <SignIn onSuccess={handleAuthSuccess} />
-      )}
+      <>
+        <InputToggle
+          label="Habilitar extensão"
+          value={isExtensionEnabled}
+          onChange={handleToggleChange}
+        />
+        <Tabs tabs={tabData} />
+      </>
     </div>
-  );
+  )
 }
 
-export default IndexPopup;
+export default IndexPopup
